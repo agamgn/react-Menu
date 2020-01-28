5c2252adc49e0001406f7c8a05d1b3e58a0026d7
agamgn <agamgn@163.com>
2020-01-28 14:52:30 +0800

    ''

---
+++ src/pages/home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./index.css";
+
+class Home extends React.Component{
+    render(){
+        return (
+            <div>
+                <button className="btn btn-success">Home</button>
+            </div>
+        );
+    }
+}
+
+export default Home;
